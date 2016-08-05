e7164bd555dc66a27093e25a897eb3c82c594cff
Robin Radic <robin@radic.nl>
2016-08-05 23:29:02 +0200

    upd

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Laradic\Phpstorm\Autocomplete\AutocompleteServiceProvider::class
     ],
 
     /*
